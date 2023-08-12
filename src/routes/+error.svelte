fd71f6df7476b32055123f3a7229be714f2f2737
cubedhuang <99danielh@gmail.com>
2023-08-12 14:35:53 -0400

    fix: add wrapper to error layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,8 +2,10 @@
 	import { page } from '$app/stores';
 </script>
 
-<h1>Error: {$page.status}</h1>
+<main class="wrapper">
+	<h1>Error: {$page.status}</h1>
 
-<p class="mt-4 text-center">{$page.error?.message}</p>
+	<p class="mt-4 text-center">{$page.error?.message}</p>
 
-<a href="/" class="mt-4 w-fit block mx-auto button button-blue">Home</a>
+	<a href="/" class="mt-4 w-fit block mx-auto button button-blue">Home</a>
+</main>
