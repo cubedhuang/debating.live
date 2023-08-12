80d500819d7e8109c3b3a106db6ca7474ba9d20a
cubedhuang <99danielh@gmail.com>
2023-08-12 14:36:17 -0400

    feat: populate about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,27 @@
 <main class="wrapper">
 	<h1>About</h1>
 
-	<div class="mt-4 w-full mx-auto max-w-prose">Test</div>
+	<div class="mt-8 w-full mx-auto max-w-prose">
+		<p>
+			<strong>debating.live</strong> is a simple platform that lets you share
+			timers for speeches and prep time between debaters, judges, and spectators
+			in a debate round. Currently, it only supports sharing time in public
+			forum debate.
+		</p>
+
+		<p class="mt-4">
+			This website and the WebSocket backend are an open source SvelteKit
+			project. You can find the source code on
+			<a
+				href="https://github.com/cubedhuang/debating.live"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="text-blue-500 hocus-visible:text-blue-700 transition font-semibold"
+			>
+				GitHub</a
+			>.
+		</p>
+
+		<p class="mt-4">This project is currently a work in progress.</p>
+	</div>
 </main>
